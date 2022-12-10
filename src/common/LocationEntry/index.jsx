34bb46080033c767ccf5fb256c6f192615aebb05
Sebastien Mesili <sebastien@mesili.fr>
2022-12-11 07:33:54 +0900

    Updated filter comparison and splitted filtering / sorting logic

--- src/common/LocationEntry/index.jsx
+++ src/common/LocationEntry/index.jsx
@@ -7,10 +7,15 @@ import './style.scss'
 export const LocationEntry = ({name, systems={}}) => {
 
     const { modes } = useSelector(state => state.servers)
+
     const filterMode = e => {
         const current_mode = systems[e].mod_id || systems[e].mode
-        return undefined === modes[current_mode] || true === modes[current_mode]
+        return true === modes[current_mode]
     }
+
+    const filteredSystems = Object.keys(systems)
+        .filter(filterMode)
+        .sort((a,b) => parseInt(systems[b].players) - parseInt(systems[a].players))
 
     return (
         <section className="location">
@@ -27,14 +32,13 @@ export const LocationEntry = ({name, systems={}}) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {Object.keys(systems)
-                            .filter(filterMode)
-                            .sort((a,b) => parseInt(systems[b].players) - parseInt(systems[a].players))
-                                .map((e,i) => <SystemEntry key={i} region={name} idx={i} />)}
-                            </tbody>
-                        </table>
-                    </section>
-                            )
+                    {filteredSystems.map((e,i) => (
+                        <SystemEntry key={i} region={name} {...systems[e]} />
+                    ))}
+                </tbody>
+            </table>
+        </section>
+        )
 }
 
 export default LocationEntry
